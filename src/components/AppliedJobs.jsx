54f10ceb276f80eaa73f19fe962d7fa6e7cb91b3
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-04-11 03:41:56 +0600

    Apply Job Page test

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AllJobCard from "./Card/AllJobCard";
 
 const AppliedJobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const allJobs = JSON.parse(localStorage.getItem("applied-jobs"));
+    if (allJobs) {
+      fetch("../../public/job_list.json")
+        .then((data) => data.json())
+        .then((data) => {
+          console.log(data);
+          setJobs(allJobs.map((e) => data.find((d) => d.id == e)));
+        });
+    }
+  }, []);
+
   return (
     <div>
       <h2>AppliedJobs</h2>
+      {jobs && jobs.map((e) => <AllJobCard key={e.id} job={e} />)}
     </div>
   );
 };
